55d65d34136f2f9c3e76e4ec4e8cd8b59c6f7d32
mait <285182060@qq.com>
2020-04-25 21:45:30 +0800

    init

---
+++ controllers/default.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+	c.Ctx.WriteString("hello,world")
+	beego.AppConfig.String("mysqluser")
+
+}
+
+func (c *MainController) GetHelloWorld() {
+	c.Ctx.WriteString("hello,world")
+}
