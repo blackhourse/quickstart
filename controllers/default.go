9c5b0064ec661b44b49a902159eadd0864c55f7b
mait <285182060@qq.com>
2020-05-07 21:03:51 +0800

    login & register

--- controllers/default.go
+++ controllers/default.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"quickstart/models"
 )
 
 type MainController struct {
@@ -11,12 +13,59 @@ type MainController struct {
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
-	c.Ctx.WriteString("hello,world")
+	//c.TplName = "index.tpl"
+	c.TplName = "register.html"
+	//c.Ctx.WriteString("hello,world")
 	beego.AppConfig.String("mysqluser")
 
 }
 
-func (c *MainController) GetHelloWorld() {
-	c.Ctx.WriteString("hello,world")
+func (c *MainController) Post() {
+	userName := c.GetString("userName")
+	pwd := c.GetString("pwd")
+	if userName == "" || pwd == "" {
+		beego.Info("数据不能为空")
+		c.Redirect("/register", 302)
+		return
+	}
+	o := orm.NewOrm()
+	user := models.User{}
+	user.Name = userName
+	user.Pwd = pwd
+	_, err := o.Insert(&user)
+	if err != nil {
+		beego.Info("插入数据失败")
+		c.Redirect("/register", 302)
+		return
+	}
+	c.Ctx.WriteString("注册成功")
+}
+
+func (c *MainController) ShowLogin() {
+	c.TplName = "login.html"
+}
+func (c *MainController) HandleLogin() {
+	userName := c.GetString("userName")
+	pwd := c.GetString("pwd")
+	if userName == "" || pwd == "" {
+		beego.Info("数据不能为空")
+		c.Redirect("/login.html", 302)
+		return
+	}
+
+	user := models.User{Name: userName}
+	o := orm.NewOrm()
+	err := o.Read(&user, "Name")
+	if err != nil {
+		beego.Info("没有此用户")
+		c.TplName = "/login.html"
+		return
+	}
+	c.Ctx.WriteString("登录成功")
+
+}
+
+// 首页
+func (c *MainController) ShowIndex() {
+	c.TplName = "/index.html"
 }
--- routers/router.go
+++ routers/router.go
@@ -7,4 +7,8 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
+	beego.Router("/register", &controllers.MainController{})
+	beego.Router("/login", &controllers.MainController{}, "get:ShowLogin;post:HandleLogin")
+	beego.Router("/index", &controllers.MainController{}, "get:ShowIndex")
+
 }
